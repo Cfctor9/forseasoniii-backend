ba8158776c1bd785adf266437fd7a3cffe43f6ac
Yeonjae Choi <fox2yj@naver.com>
2023-07-17 19:21:29 +0900

    first commit

---
+++ src/main/java/com/iii/forseasoniii/ForseasoniiiApplication.java
@@ -0,0 +1,13 @@
+package com.iii.forseasoniii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForseasoniiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForseasoniiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iii/forseasoniii/ForseasoniiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iii.forseasoniii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForseasoniiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
